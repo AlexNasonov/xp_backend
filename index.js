73401b7acced5e7842da51959d0755c5e7332f76
Max Lapshin <max@maxidoors.ru>
2019-07-29 18:18:25 +0300

    raise body parser to 500 mb

--- index.js
+++ index.js
@@ -92,8 +92,8 @@ module.exports = (rootPath, certificate) => {
   app.set('view engine', 'ejs');
 
   app.use(morganLogger('dev'));
-  app.use(bodyParser.json({limit: '50mb'}));
-  app.use(bodyParser.urlencoded({limit: '50mb', extended: true, parameterLimit: 50000}));
+  app.use(bodyParser.json({limit: '500mb'}));
+  app.use(bodyParser.urlencoded({limit: '500mb', extended: true, parameterLimit: 50000}));
   app.use(cookieParser(config.get('appName')));
   app.use('/public', express.static(paths.public, {fallthrough: false}));
 
