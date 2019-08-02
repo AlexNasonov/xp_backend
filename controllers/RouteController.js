ea10175d2aee61f8402a802f23f5ca8f3ea2188a
nasonov.alexey@gmail.com <47k11884>
2019-08-02 21:42:49 +0300

    Route Controller

---
+++ controllers/RouteController.js
@@ -0,0 +1,270 @@
+const models = require('../modules/sequelize');
+
+const RedC = require('./RedirectController');
+
+const LocalLogger = require('../modules/logger');
+const log = new LocalLogger(module);
+
+const config = require('../modules/config');
+const host = config.getEnv('host');
+const mirrors = config.get('mirrors');
+const locales = config.get('locales');
+const regions = config.get('regions');
+const subdomains = config.get('subdomains');
+const selTags = config.get('articlesSelectedTags') || [];
+const blogTags = config.get('articlesBlogTags');
+const searchTags = config.get('articlesSearchTags');
+
+module.exports = class RouteController {
+  static prepareLocaleSet(prefix, strict) {
+    const list = [];
+    const pr = `${prefix || ''}${(strict) ? '':'*'}`;
+    for (const r of regions) {
+      for (const l of locales) {
+        list.push(`/${l}-${r}/${pr}`);
+      }
+    }
+    list.push(`/${pr}`);
+    return list;
+  };
+
+  static setLRUrl(hostname, path) {
+    let locale = locales[0];
+    let region = regions[0];
+    let tagPos = 1;
+
+    const sPath = path.split('/');
+
+    // set locale and region
+    if (hostname !== host) {
+      for (const i of mirrors) {
+        if (i[0] === hostname) {
+          const lr = i[1].split('-');
+          locale = lr[0];
+          region = lr[1];
+        }
+      }
+    } else {
+      let lr = sPath[1];
+      lr = lr.split('-');
+      if (lr.length === 2 && locales.includes(lr[0]) && regions.includes(lr[1])) {
+        locale = lr[0];
+        region = lr[1];
+        tagPos = 2;
+      }
+    }
+
+    // exclude articlesSelectedTags tag from url to get a clear article url
+    const tag = sPath[tagPos];
+    if (selTags.includes(tag) && !path.endsWith(tag)) tagPos++;
+    const url = '/'+sPath.slice(tagPos).join('/');
+    return [locale.toLowerCase(), region.toLowerCase(), url.toLowerCase()];
+  };
+
+  static setParams(url, locale, sd) {
+    const params = {where: {
+      locale: locale || locales[0],
+      subdomain: (sd && sd.length>0 && subdomains.includes(sd[0]))? sd[0] : subdomains[0],
+    }};
+    if (url) params.where.url = url;
+    return params;
+  };
+
+  static setAlts(locale, url) {
+    const l = [];
+    for (const i of regions) l.push(`/${locale}-${i}${url}`);
+    return l;
+  };
+
+  static setData(region, locale, instance, host, url, path) {
+    let h; let p;
+    if (process.env.NODE_ENV === 'development' || process.env.TUNNEL_ENV) {
+      h = config.get('host')['development']+':'+config.get('port');
+      p = 'http://';
+    } else {
+      h = host;
+      p = (process.env.NODE_ENV === 'staging') ? 'http://' : 'https://';
+    }
+    return {
+      region: region,
+      locale: locale,
+      title: instance.title,
+      description: instance.description,
+      keywords: instance.keywords,
+      og_image_url: instance.og_image_url,
+      og_image_height: instance.og_image_height,
+      og_image_width: instance.og_image_width,
+      css: instance.css,
+      host: h,
+      url: url,
+      path: path,
+      base_url: p+h+path.replace(url, ''),
+      alts: RouteController.setAlts(locale, url),
+      details: instance.details,
+    };
+  };
+
+  static err404() {
+    const e = new Error();
+    e.status = 404;
+    return e;
+  };
+
+
+  static async setGeneratedPage(subdomains, pageNumber, region, locale, host, url, limit, path) {
+    const params = RouteController.setParams(false, locale, subdomains);
+    const pn = (pageNumber) ? parseInt(pageNumber)-1 : 0;
+    params.limit = limit;
+    params.offset = pn*params.limit;
+    params.order = [['createdAt', 'DESC']];
+
+    const page = await models.Page.findOne({where: {
+      url: url,
+    }});
+
+    if (!page || !page.published) throw RouteController.err404();
+
+    const res = {
+      params: params,
+      data: RouteController.setData(region, locale, page, host, url, path),
+    };
+    res.data.limit = limit;
+
+    return res;
+  }
+
+  static async setBlogPage(subdomains, pageNumber, region, locale, host, url, tagFilter, path) {
+    const preset = await RouteController.setGeneratedPage(subdomains, pageNumber, region, locale, host, url, 20, path);
+    const data = preset.data;
+    if (blogTags) {
+      const tagList = (tagFilter && blogTags.includes(tagFilter)) ? [tagFilter] : blogTags;
+
+      preset.params.include = [{
+        model: models.Tag,
+        attributes: ['id'],
+        where: {id: tagList},
+        through: {attributes: []},
+      }];
+    }
+    data.content = await models.Article.findAndCountAll(preset.params);
+
+    return data;
+  };
+
+
+  static async setSearchPage(subdomains, pageNumber, region, locale, host, url, tags, q, path) {
+    const preset = await RouteController.setGeneratedPage(subdomains, pageNumber, region, locale, host, url, 20, path);
+    const data = preset.data;
+    const params = preset.params;
+    params.include = [{
+      model: models.Tag,
+      attributes: ['id'],
+      through: {attributes: []},
+      where: {id: (tags)? JSON.parse(tags) : searchTags},
+    }];
+
+    const results = await models.sequelize.query(`
+    SELECT *
+    FROM ${models.Article.tableName}
+    WHERE _search @@ plainto_tsquery('english', :query);
+  `, {
+      model: models.Article,
+      replacements: {query: q},
+    });
+
+    const ids = [];
+
+    for (const i of results) {
+      ids.push(i.id);
+    }
+
+    params.where = {
+      id: ids,
+      locale: locale,
+    };
+
+    data.content = await models.Article.findAndCountAll(params);
+    data.content.query = q;
+    return data;
+  }
+
+  static async findRedirect(url, locale, subdomain) {
+    const redirect = await RedC.find(url, locale, subdomain);
+    if (redirect) return {redirect: redirect.new};
+    else throw RouteController.err404();
+  }
+
+  static async setArticlePage(subdomains, region, locale, host, url, tag, path) {
+    const params = RouteController.setParams(url, locale, subdomains);
+    params.include = [{
+      model: models.Page,
+      attributes: ['css'],
+    },
+    {
+      model: models.Tag,
+      attributes: ['id'],
+      through: {attributes: []},
+      where: {id: tag},
+    }];
+
+    const uri = '/'+tag+url;
+
+    const article = await models.Article.findOne(params);
+
+    if (!article) return await RouteController.findRedirect(uri, params.where.locale, params.where.subdomain);
+    if (!article.published) throw RouteController.err404();
+
+    const data = RouteController.setData(region, locale, article, host, uri, path);
+    data.content = article.body;
+    data.css = article.page.css;
+    data.pageId = article.pageId;
+    return data;
+  };
+
+  static async setCustomPage(subdomains, region, locale, host, url, path) {
+    const params = RouteController.setParams(url, locale, subdomains);
+    const page = await models.Page.findOne(params);
+
+    if (!page) return await RouteController.findRedirect(url, params.where.locale, params.where.subdomain);
+    if (!page.published) throw RouteController.err404();
+
+    const data = RouteController.setData(region, locale, page, host, url, path);
+    data.pageId = page.id;
+    return data;
+  };
+
+
+  static async setIndexPage(subdomains, region, locale, host, url, path) {
+    const page = await models.Page.findOne(RouteController.setParams(url, locale, subdomains));
+    if (!page || !page.published) throw RouteController.err404();
+
+    const data = RouteController.setData(region, locale, page, host, url, path);
+    data.pageId = page.id;
+
+    const promo = await models.Tag.findByPk('promo', {include: [{
+      model: models.Article,
+      where: {locale: locale},
+      attributes: ['url', 'header', 'summary', 'og_image_url', 'createdAt'],
+      through: {attributes: []},
+    }],
+    order: [[models.Article, 'header', 'ASC']],
+    });
+    data.promo = (promo) ? promo.articles : undefined;
+    return data;
+  };
+
+  static render404(req, res, next) {
+    const lrData = RouteController.setLRUrl(req.hostname, req.path);
+    const [locale, region] = [lrData[0], lrData[1]];
+
+    RouteController.setCustomPage(req.subdomains, region, locale, req.hostname, '/404', req.path)
+        .then((data)=> {
+          res.status(404);
+          return res.render('pages/'+data.pageId, data);
+        })
+        .catch((e) =>{
+          log.error(e.message);
+          return next(e);
+        });
+  };
+};
--- routes/pages.js
+++ routes/pages.js
@@ -9,382 +9,14 @@ const log = new LocalLogger(module);
 
 const leadTracer = require('../modules/leads').LeadTracer;
 
-const models = require('../modules/sequelize');
-
-const RedC = require('../controllers/RedirectController');
-
 const config = require('../modules/config');
 const host = config.getEnv('host');
-const mirrors = config.get('mirrors');
-const locales = config.get('locales');
-const regions = config.get('regions');
-const subdomains = config.get('subdomains');
+
 const selTags = config.get('articlesSelectedTags') || [];
-const blogTags = config.get('articlesBlogTags');
-const searchTags = config.get('articlesSearchTags');
 
-prepareLocaleSet = (prefix, strict) => {
-  const list = [];
-  const pr = prefix || '';
-  for (const r of regions) {
-    for (const l of locales) {
-      let lr = `/${l}-${r}/${pr}`;
-      if (!strict) lr = lr+'*';
-      list.push(lr);
-    }
-  }
-  return list;
-};
-
-setDefLR = (h) => {
-  let lr = [locales[0], regions[0]];
-  if (h !== host) {
-    for (const i of mirrors) {
-      if (i[0] === h) lr = i[1].split('-');
-    }
-  }
-  return lr;
-};
-
-setLRUrl = (reqPath, slice) =>{
-  const sPath = reqPath.split('/');
-  const [locale, region] = sPath[1].split('-');
-  const url = '/'+sPath.slice(slice).join('/');
-  return [locale, region, url];
-};
-
-setParams = (url, locale, sd) => {
-  const params = {where: {
-    locale: locale || locales[0],
-    subdomain: (sd && sd.length>0 && subdomains.includes(sd[0]))? sd[0] : subdomains[0],
-  }};
-  if (url) params.where.url = url;
-  return params;
-};
-
-setAlts = (locale, url) => {
-  const l = [];
-  for (const i of regions) l.push(`/${locale}-${i}${url}`);
-  return l;
-};
-
-setData = (region, locale, instance, host, url) => {
-  let h; let p;
-  if (process.env.NODE_ENV === 'development' || process.env.TUNNEL_ENV) {
-    h = config.get('host')['development']+':'+config.get('port');
-    p = 'http://';
-  } else {
-    h = host;
-    p = (process.env.NODE_ENV === 'staging') ? 'http://' : 'https://';
-  }
-  return {
-    region: region,
-    locale: locale,
-    title: instance.title,
-    description: instance.description,
-    keywords: instance.keywords,
-    og_image_url: instance.og_image_url,
-    og_image_height: instance.og_image_height,
-    og_image_width: instance.og_image_width,
-    css: instance.css,
-    host: h,
-    url: url,
-    base_url: p+h,
-    alts: setAlts(locale, url),
-    altLocale: (instance.details) ? instance.details.altLocale : undefined,
-    details: instance.details,
-  };
-};
-
-err404 = () => {
-  const e = new Error();
-  e.status = 404;
-  return e;
-};
-
-render404 = (req, res, next, localeDefined) =>{
-  const lrData = (localeDefined)? setLRUrl(req.path, 2) : setDefLR(req.hostname);
-  const [locale, region] = [lrData[0], lrData[1]];
-
-  setCustomPage(req.subdomains, region, locale, req.hostname, '/404')
-      .then((data)=> {
-        res.status(404);
-        return res.render('pages/'+data.pageId, data);
-      })
-      .catch((e) =>{
-        log.error(e.message);
-        return next(e);
-      });
-};
-
-async function setGeneratedPage(subdomains, pageNumber, region, locale, host, url, limit) {
-  const params = setParams(false, locale, subdomains);
-  const pn = (pageNumber) ? parseInt(pageNumber)-1 : 0;
-  params.limit = limit;
-  params.offset = pn*params.limit;
-  params.order = [['createdAt', 'DESC']];
-
-  const page = await models.Page.findOne({where: {
-    url: url,
-  }});
-  if (!page || !page.published) throw err404();
-
-  const res = {
-    params: params,
-    data: setData(region, locale, page, host, url),
-  };
-  res.data.limit = limit;
-
-  return res;
-}
-
-async function setBlogPage(subdomains, pageNumber, region, locale, host, url, tagFilter) {
-  const preset = await setGeneratedPage(subdomains, pageNumber, region, locale, host, url, 20);
-  const data = preset.data;
-  if (blogTags) {
-    const tagList = (tagFilter && blogTags.includes(tagFilter)) ? [tagFilter] : blogTags;
-
-    preset.params.include = [{
-      model: models.Tag,
-      attributes: ['id'],
-      where: {id: tagList},
-      through: {attributes: []},
-    }];
-  }
-  data.content = await models.Article.findAndCountAll(preset.params);
-
-  return data;
-};
+const RouteC = require('../controllers/RouteController');
 
 
-async function setSearchPage(subdomains, pageNumber, region, locale, host, url, tags, q) {
-  const preset = await setGeneratedPage(subdomains, pageNumber, region, locale, host, url, 20);
-  const data = preset.data;
-  const params = preset.params;
-  params.include = [{
-    model: models.Tag,
-    attributes: ['id'],
-    through: {attributes: []},
-    where: {id: (tags)? JSON.parse(tags) : searchTags},
-  }];
-
-  const results = await models.sequelize.query(`
-    SELECT *
-    FROM ${models.Article.tableName}
-    WHERE _search @@ plainto_tsquery('english', :query);
-  `, {
-    model: models.Article,
-    replacements: {query: q},
-  });
-
-  const ids = [];
-
-  for (const i of results) {
-    ids.push(i.id);
-  }
-
-  params.where = {
-    id: ids,
-    locale: locale,
-  };
-
-  data.content = await models.Article.findAndCountAll(params);
-  data.content.query = q;
-  return data;
-}
-
-async function setArticlePage(subdomains, region, locale, host, url, tag) {
-  const params = setParams(url, locale, subdomains);
-  params.include = [{
-    model: models.Page,
-    attributes: ['css'],
-  },
-  {
-    model: models.Tag,
-    attributes: ['id'],
-    through: {attributes: []},
-    where: {id: tag},
-  }];
-
-  const uri = '/'+tag+url;
-
-  const article = await models.Article.findOne(params);
-
-  if (!article) {
-    const redirect = await RedC.find(uri, params.where.locale, params.where.subdomain);
-
-    if (redirect) return {redirect: redirect.new};
-    else throw err404();
-  } else if (!article.published) throw err404();
-  const data = setData(region, locale, article, host, uri);
-  data.content = article.body;
-  data.css = article.page.css;
-  data.pageId = article.pageId;
-  return data;
-};
-
-async function setCustomPage(subdomains, region, locale, host, url) {
-  const params = setParams(url, locale, subdomains);
-  const page = await models.Page.findOne(params);
-  if (!page) {
-    const redirect = await RedC.find(url, params.where.locale, params.where.subdomain);
-    if (redirect) return {redirect: redirect.new};
-    else throw err404();
-  } else if (!page.published) throw err404();
-
-  const data = setData(region, locale, page, host, url);
-  data.pageId = page.id;
-  return data;
-};
-
-
-async function setIndexPage(subdomains, region, locale, host, url) {
-  const page = await models.Page.findOne(setParams(url, locale, subdomains));
-  if (!page || !page.published) throw err404();
-
-  const data = setData(region, locale, page, host, url);
-  data.pageId = page.id;
-
-  const promo = await models.Tag.findByPk('promo', {include: [{
-    model: models.Article,
-    where: {locale: locale},
-    attributes: ['url', 'header', 'summary', 'og_image_url', 'createdAt'],
-    through: {attributes: []},
-  }],
-  order: [[models.Article, 'header', 'ASC']],
-  });
-  data.promo = (promo) ? promo.articles : undefined;
-  return data;
-};
-
-
-/**
- * LOCALE DEFINED IN URL
- */
-
-router.get(prepareLocaleSet('blog', true), leadTracer, (req, res, next) => {
-  if (![req.hostname, 'www.'+req.hostname].includes(host)) return render404(req, res, next, true);
-  const [locale, region, url] = setLRUrl(req.path, 2);
-  setBlogPage(req.subdomains, req.query.page, region, locale, req.hostname, url, req.query.tag)
-      .then((data)=> {
-        const d = data;
-        d.base_url = data.base_url + `/`+data.locale+'-'+data.region;
-        return res.render('pages/blog', d);
-      })
-      .catch((e) =>{
-        if (e.status === 404) return render404(req, res, next, true);
-        else {
-          log.error(e.message);
-          return next(e);
-        }
-      });
-});
-
-
-router.get(prepareLocaleSet('search', true), leadTracer, (req, res, next) => {
-  if (![req.hostname, 'www.'+req.hostname].includes(host)) return render404(req, res, next, true);
-  const [locale, region, url] = setLRUrl(req.path, 2);
-  setSearchPage(req.subdomains, req.query.page, region, locale, req.hostname, url, req.query.tags, req.query.q)
-      .then((data)=> {
-        const d = data;
-        d.base_url = data.base_url + `/`+data.locale+'-'+data.region;
-        return res.render('pages/search', d);
-      })
-      .catch((e) =>{
-        if (e.status === 404) return render404(req, res, next, true);
-        else {
-          log.error(e.message);
-          return next(e);
-        }
-      });
-});
-
-
-for (const tag of selTags) {
-  router.get(prepareLocaleSet(tag+'/'), leadTracer, (req, res, next) => {
-    if (![req.hostname, 'www.'+req.hostname].includes(host)) return render404(req, res, next, true);
-    const [locale, region, url] = setLRUrl(req.path, 3);
-    setArticlePage(req.subdomains, region, locale, req.hostname, url, tag)
-        .then((data)=> {
-          if (data.redirect) return res.redirect(data.redirect);
-          const d = data;
-          d.base_url = data.base_url + `/`+data.locale+'-'+data.region;
-          return res.render('pages/'+data.pageId, d);
-        })
-        .catch((e) =>{
-          if (e.status === 404) return render404(req, res, next, true);
-          else {
-            log.error(e.message);
-            return next(e);
-          }
-        });
-  });
-}
-
-
-router.get(prepareLocaleSet(), leadTracer, (req, res, next) => {
-  if (![req.hostname, 'www.'+req.hostname].includes(host)) return render404(req, res, next, true);
-  const [locale, region, url] = setLRUrl(req.path, 2);
-  if (url === '/') {
-    setIndexPage(req.subdomains, region, locale, req.hostname, url)
-        .then((data)=> {
-          const d = data;
-          d.base_url = data.base_url + `/`+data.locale+'-'+data.region;
-          return res.render('pages/'+data.pageId, d);
-        })
-        .catch((e) =>{
-          if (e.status === 404) return render404(req, res, next, true);
-          else {
-            log.error(e.message);
-            return next(e);
-          }
-        });
-  } else {
-    setCustomPage(req.subdomains, region, locale, req.hostname, url)
-        .then((data)=> {
-          if (data.redirect) return res.redirect(data.redirect);
-          const d = data;
-          d.base_url = data.base_url + `/`+data.locale+'-'+data.region;
-          return res.render('pages/'+data.pageId, d);
-        })
-        .catch((e) =>{
-          if (e.status === 404) return render404(req, res, next, true);
-          else {
-            log.error(e.message);
-            return next(e);
-          }
-        });
-  }
-});
-
-/**
- * LOCALE NOT DEFINED IN URL
- */
-router.get('/blog', leadTracer, (req, res, next) => {
-  const [locale, region] = setDefLR(req.hostname);
-  setBlogPage(req.subdomains, req.query.page, region, locale, req.hostname, '/blog', req.query.tag)
-      .then((data)=> res.render('pages/blog', data))
-      .catch((e) =>{
-        if (e.status === 404) return render404(req, res, next);
-        else {
-          log.error(e.message);
-          return next(e);
-        }
-      });
-});
-
-router.get('/search', leadTracer, (req, res, next) => {
-  const [locale, region] = setDefLR(req.hostname);
-  setSearchPage(req.subdomains, req.query.page, region, locale, req.hostname, '/search', req.query.tags, req.query.q)
-      .then((data)=> res.render('pages/search', data))
-      .catch((e) =>{
-        if (e.status === 404) return render404(req, res, next);
-        else {
-          log.error(e.message);
-          return next(e);
-        }
-      });
-});
 
 router.get('/sitemap.xml', leadTracer, (req, res, next) => {
   const hn = req.hostname;
@@ -393,25 +25,6 @@ router.get('/sitemap.xml', leadTracer, (req, res, next) => {
       : path.join(process.env.publicPath, `./files/sitemaps/sitemap.xml`);
   res.sendFile(file);
 });
-
-for (const tag of selTags) {
-  router.get('/'+tag+'/*', leadTracer, (req, res, next) => {
-    const [locale, region] = setDefLR(req.hostname);
-    const url = '/' + req.path.split('/').slice(2).join('/');
-    setArticlePage(req.subdomains, region, locale, req.hostname, url, tag).
-        then((data) => {
-          if (data.redirect) return res.redirect(data.redirect);
-          else res.render('pages/' + data.pageId, data);
-        }).
-        catch((e) => {
-          if (e.status === 404) return render404(req, res, next);
-          else {
-            log.error(e.message);
-            return next(e);
-          }
-        });
-  });
-}
 
 router.get('/robots.txt', async (req, res, next) => {
   const hn = req.hostname;
@@ -424,13 +37,15 @@ router.get('/robots.txt', async (req, res, next) => {
   res.sendFile(file);
 });
 
-router.get('/', leadTracer, (req, res, next) => {
-  const [locale, region] = setDefLR(req.hostname);
-  const url = req.path;
-  setIndexPage(req.subdomains, region, locale, req.hostname, url)
-      .then((data)=> res.render('pages/'+data.pageId, data))
+// blog pages
+router.get(RouteC.prepareLocaleSet('blog', true), leadTracer, (req, res, next) => {
+  const [locale, region, url] = RouteC.setLRUrl(req.hostname.toLowerCase(), req.path.toLowerCase());
+  RouteC.setBlogPage(req.subdomains, req.query.page, region, locale, req.hostname, '/blog', req.query.tag, req.path)
+      .then((data)=> {
+        res.render('pages/blog', data);
+      })
       .catch((e) =>{
-        if (e.status === 404) return render404(req, res, next);
+        if (e.status === 404) return RouteC.render404(req, res, next);
         else {
           log.error(e.message);
           return next(e);
@@ -438,16 +53,63 @@ router.get('/', leadTracer, (req, res, next) => {
       });
 });
 
-router.get('/*', leadTracer, (req, res, next) => {
-  const [locale, region] = setDefLR(req.hostname);
-  const url = req.path;
-  setCustomPage(req.subdomains, region, locale, req.hostname, url)
+// search pages
+router.get(RouteC.prepareLocaleSet('search', true), leadTracer, (req, res, next) => {
+  const [locale, region, url] = RouteC.setLRUrl(req.hostname.toLowerCase(), req.path.toLowerCase());
+  RouteC.setSearchPage(req.subdomains, req.query.page, region, locale, req.hostname, url, req.query.tags, req.query.q, req.path)
+      .then((data)=> {
+        return res.render('pages/search', data);
+      })
+      .catch((e) =>{
+        if (e.status === 404) return RouteC.render404(req, res, next, true);
+        else {
+          log.error(e.message);
+          return next(e);
+        }
+      });
+});
+
+// tag-defined pages like:  /post/post-name
+for (const tag of selTags) {
+  router.get(RouteC.prepareLocaleSet(tag+'/'), leadTracer, (req, res, next) => {
+    const [locale, region, url] = RouteC.setLRUrl(req.hostname.toLowerCase(), req.path.toLowerCase());
+    RouteC.setArticlePage(req.subdomains, region, locale, req.hostname, url, tag, req.path)
+        .then((data)=> {
+          if (data.redirect) return res.redirect(data.redirect);
+          return res.render('pages/'+data.pageId, data);
+        })
+        .catch((e) =>{
+          if (e.status === 404) return RouteC.render404(req, res, next, true);
+          else {
+            log.error(e.message);
+            return next(e);
+          }
+        });
+  });
+}
+
+router.get(RouteC.prepareLocaleSet('', true), leadTracer, (req, res, next) => {
+  const [locale, region, url] = RouteC.setLRUrl(req.hostname.toLowerCase(), req.path.toLowerCase());
+  RouteC.setIndexPage(req.subdomains, region, locale, req.hostname, '/', '/')
+      .then((data)=> res.render('pages/'+data.pageId, data))
+      .catch((e) =>{
+        if (e.status === 404) return RouteC.render404(req, res, next);
+        else {
+          log.error(e.message);
+          return next(e);
+        }
+      });
+});
+
+router.get(RouteC.prepareLocaleSet(''), leadTracer, (req, res, next) => {
+  const [locale, region, url] = RouteC.setLRUrl(req.hostname.toLowerCase(), req.path.toLowerCase());
+  RouteC.setCustomPage(req.subdomains, region, locale, req.hostname, url, req.path)
       .then((data)=> {
         if (data.redirect) return res.redirect(data.redirect);
         else res.render('pages/'+data.pageId, data);
       })
       .catch((e) =>{
-        if (e.status === 404) return render404(req, res, next);
+        if (e.status === 404) return RouteC.render404(req, res, next);
         else {
           log.error(e.message);
           return next(e);
